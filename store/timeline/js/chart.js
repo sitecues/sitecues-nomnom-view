933a27da69be4a2ed320b00bb9b857d01a33fee3
Aaron Leventhal <aleventhal@aisquared.com>
2016-08-20 21:46:08 -0400

    Reorg direction structure

---
+++ store/timeline/js/chart.js
@@ -0,0 +1,381 @@
+// TODO clean up code
+
+'use strict';
+
+function getChartConfig(options, doEnableRatioLine, doEnableLine1, doEnableLine2) {
+  var yAxes = [],
+    doUseSameScaleForAllEvents = doEnableLine1 && !options.doStretch,
+    doUseSecondAxis = doEnableLine2 && !doUseSameScaleForAllEvents,
+    tickConfig = {
+      callback: function(value, index, values) {
+        return value.toLocaleString();
+      },
+      beginAtZero: true
+    };
+
+  if (doEnableLine1) {
+    yAxes = yAxes.concat([{
+      type: 'linear',
+      id: 'y-axis-1',
+      position: 'left',
+      ticks: tickConfig,
+      scaleLabel: {
+        display: true,
+        fontColor: doUseSameScaleForAllEvents ? 'black' : 'rgba(20,20,255,1)',
+        fontSize: 14,
+        labelString: doUseSameScaleForAllEvents ? 'Events' : getLabel(options, '1')
+      }
+    }]);
+  }
+
+  if (doUseSecondAxis) {
+    yAxes = yAxes.concat({
+      type: 'linear',
+      position: 'left',
+      ticks: tickConfig,
+      scaleLabel: {
+        display: true,
+        fontColor: 'rgba(255,110,0,1)',
+        fontSize: 14,
+        labelString: getLabel(options, '2')
+      },
+      // grid line settings
+      gridLines: {
+        drawOnChartArea: false // only want the grid lines for one axis to show up
+      },
+      id: 'y-axis-2'
+    });
+  }
+
+  if (doEnableRatioLine) {
+    yAxes = yAxes.concat({
+      type: 'linear',
+      position: 'right',
+      // display: false,
+      scaleLabel: {
+        display: true,
+        labelString: 'ratio',
+        fontSize: 14
+      },
+      // grid line settings
+      gridLines: {
+        drawOnChartArea: false // only want the grid lines for one axis to show up
+      },
+      id: 'y-axis-ratio'
+    });
+  }
+
+  return {
+    // title: {
+    //   display: true,
+    //   text: 'Sitecues Metrics Chart'
+    // },
+    stacked: true,
+    scales: {
+      yAxes: yAxes,
+      xAxes: [{
+        type: 'time'
+      }],
+    },
+    time: {
+      parser: 'MM/DD/YYYY'
+    },
+    legend: {
+      labels: {
+        fontSize: 14
+      }
+    }
+  };
+}
+
+function convertDateToIndex(date, datesWithDataAvailable, defaultValueIfNotFound) {
+  var extractParts = date.split('/'),
+    month = extractParts[0],
+    day = extractParts[1],
+    year = extractParts[2],
+    dateAsInt = parseInt(year + month + day),
+    dateIndex = datesWithDataAvailable.indexOf(dateAsInt);
+
+  return dateIndex < 0 ? defaultValueIfNotFound : dateIndex;
+}
+
+function getDateLabels(startDateIndex, endDateIndex, datesWithDataAvailable) {
+  var labels = [],
+    index = startDateIndex;
+
+  function convertIndexToDate(dateIndex) {
+    var dateAsYYYYMMDD = datesWithDataAvailable[dateIndex].toString(),
+      year = dateAsYYYYMMDD.substr(2, 2),
+      month = dateAsYYYYMMDD.substr(4, 2),
+      day = dateAsYYYYMMDD.substr(6, 2);
+
+    return month + '/' + day + '/' + year;
+  }
+
+  while (index <= endDateIndex) {
+    labels.push(convertIndexToDate(index ++ ));
+  }
+
+  return labels;
+}
+
+function smoothData(origData, options) {
+  var length = origData.length,
+    smoothSize = options.doSmooth ? (options.doUltraSmooth ? 10 : 3) : 0, // +/- 3 days = 1 week vs +/- 10 days = 3 weeks
+    movingAverage = [],
+    numPointsAveragedPerPoint = 1 + smoothSize * 2; // Current point + number on each side
+  for (var index = 0; index < length; index++) {
+    var total = origData[index],
+      smoothDistance = smoothSize,
+      numPointsAveragedThisPoint = numPointsAveragedPerPoint;
+    while (smoothDistance > 0) {
+      if (index - smoothDistance >= 0) {
+        total += origData[index - smoothDistance];
+      }
+      else {
+        -- numPointsAveragedThisPoint;
+      }
+      if (index + smoothDistance < length) {
+        total += origData[index + smoothDistance];
+      }
+      else {
+        -- numPointsAveragedThisPoint;
+      }
+      -- smoothDistance;
+    }
+    movingAverage.push(toPrecision(total / numPointsAveragedThisPoint, 4));
+  }
+
+  return movingAverage;
+}
+
+// // When a data point is null, it means we are missing data
+// // This function will just copy the previous value -- so it assumes that we have good data on day 1
+// // TODO consider a special fix for July 3-5 that uses average of July 2 & 6
+// function removeHolesFromData(dataPoints, missingDays) {
+//   var copyOfDataPoints = dataPoints.slice(),
+//     length = copyOfDataPoints.length,
+//     prevValue = copyOfDataPoints[0];
+//   for (var index = 1; index < length; index ++) {
+//     if (copyOfDataPoints[index] === 0 && missingDays.indexOf(index) >= 0) {
+//       copyOfDataPoints[index] = prevValue;
+//     }
+//     else {
+//       prevValue = copyOfDataPoints[index];
+//     }
+//   }
+//
+//   return copyOfDataPoints;
+// }
+
+// which === '1'|'2' for which line in the graph
+function getDataPoints(which, data, startDateIndex, endDateIndex, options) {
+  var dataSource = getDataSource(which, data, options),
+    smoothedData;
+
+  if (dataSource) {
+    smoothedData = smoothData(dataSource, options);
+    return smoothedData.slice(startDateIndex, endDateIndex + 1);
+  }
+}
+
+// which === '1'|'2' for which line in the graph
+function getDataSource(which, data, options) {
+  var eventName = options['event' + which],
+    uaName = options['ua' + which],
+    location = options['loc' + which],
+    eventTotals = data.eventTotals,
+    eventMap = eventTotals.byLocation[location],
+    dataSource = eventMap && eventMap[eventName] && eventMap[eventName][uaName];
+
+  return dataSource || [];
+}
+
+function toPrecision(val, precision) {
+  return parseFloat(val.toPrecision(precision));
+}
+
+function getRatioDataPoints(data1, data2, options) {
+  var dataPoints = data1.map(function(value, index) {
+    return value ? toPrecision(data2[index] / value, 4) : null; // null means skip this data point -- no data
+  });
+  return smoothData(dataPoints, options);
+}
+
+function getTotal(data) {
+  if (!data) {
+    return 0;
+  }
+
+  function sum(a, b) {
+    return a + b;
+  }
+
+  return data.reduce(sum, 0);
+}
+
+function getLabel(options, which) {
+  var eventName = options['event' + which],
+    uaName = options['ua' + which],
+    locName = options['loc' + which],
+    labelParts = [];
+
+  if (options.event1 !== options.event2) {
+    labelParts.push(eventName);
+  }
+  if (options.ua1 !== options.ua2) {
+    labelParts.push('using ' + uaName);
+  }
+  if (options.loc1 !== options.loc2) {
+    labelParts.push('on ' + locName);
+  }
+
+  return labelParts.join(' ');
+}
+
+function updateSummaryBox(total1, total2, avg1, avg2, avgRatio) {
+  function toStr(num) {
+    return num ? Math.round(num).toLocaleString() : '';
+  }
+  $('#total1').text(toStr(total1));
+  $('#total2').text(toStr(total2));
+  $('#avg1').text(toStr(avg1));
+  $('#avg2').text(toStr(avg2));
+  $('#avg-ratio').text(avgRatio ? avgRatio.toPrecision(3): '');
+}
+
+function createChartView(data, options) {
+  var
+    datesWithDataAvailable = data.summary.config.dates,
+    startDateIndex = convertDateToIndex(options.startDate, datesWithDataAvailable, 0),
+    endDateIndex = convertDateToIndex(options.endDate, datesWithDataAvailable, data.summary.config.dates.length - 1),
+    data1 = getDataPoints('1', data, startDateIndex, endDateIndex, options),
+    data2 = getDataPoints('2', data, startDateIndex, endDateIndex, options),
+    numDays = data1.length || data2.length,
+    total1 = getTotal(data1),
+    total2 = getTotal(data2),
+    avg1 = total1 / numDays,
+    avg2 = total2 / numDays,
+    avgRatio = total1 ? total2 / total1 : 0,
+    datasets = [],
+    doEnableLine1 = options.doEnableLine1 && data1.length,
+    doEnableLine2 = options.doEnableLine2 && data2.length,
+    doEnableRatioLine = doEnableLine1 && doEnableLine2 &&
+      (options.event2 !== options.event1 ||
+      options.ua2 !== options.ua1 ||
+      options.loc2 !== options.loc1),
+    doEnableAverageLines = false,
+    yAxis2;
+
+  updateSummaryBox(total1, total2, avg1, avg2, avgRatio);
+
+  if (doEnableLine1) {
+    datasets = datasets.concat({
+      label: getLabel(options, '1'),
+      backgroundColor: 'rgba(20,20,255,0.1)',
+      borderColor: 'rgba(20,20,255,.4)',
+      fill: true,
+      pointHitRadius: 10,
+      data: data1 || [0],
+      yAxisID: 'y-axis-1'
+    });
+    if (doEnableAverageLines) {
+      datasets = datasets.concat({
+        label: 'average: ' + toPrecision(total1 / numDays, 4).toLocaleString() + ' (' + total1.toLocaleString() + ' total)     ',
+        backgroundColor: 'rgba(0,0,0,0)',
+        pointBorderColor: 'rgba(0,0,0,0)',
+        borderColor: 'rgba(20,20,255,.4)',
+        borderDash: [10, 5],
+        fill: false,
+        pointHitRadius: 0,
+        data: new Array(numDays).fill(toPrecision(avg1, 4)),
+        yAxisID: 'y-axis-1'
+      });
+    }
+  }
+
+  // If event2 is different, add it as a dataset as well as ration between the two
+  if (doEnableLine2) {
+    yAxis2 = (options.doStretch || !doEnableLine1) ? 'y-axis-2' : 'y-axis-1';
+    datasets = datasets.concat({
+      label: getLabel(options, '2'),
+      borderColor: 'rgba(255,110,0,.5)',
+      backgroundColor: 'rgba(255,110,0,0.1)',
+      pointHitRadius: 10,
+      data: data2 || [0],
+      yAxisID: yAxis2
+    });
+    if (doEnableAverageLines) {
+      datasets = datasets.concat({
+        label: 'average: ' + toPrecision(total2 / numDays, 4).toLocaleString() + ' (' + total2.toLocaleString() + ' total)     ',
+        backgroundColor: 'rgba(0,0,0,0)',
+        borderColor: 'rgba(255,110,0,.4)',
+        pointBorderColor: 'rgba(0,0,0,0)',
+        borderDash: [10, 5],
+        pointHitRadius: 0,
+        data: new Array(numDays).fill(toPrecision(avg2, 4)),
+        yAxisID: yAxis2
+      });
+    }
+  }
+
+  if (doEnableRatioLine) {
+    datasets = datasets.concat({
+      label: 'ratio #2/#1', //[average = ' + (total1 / total2).toFixed(4) + ']',
+      data: getRatioDataPoints(data1, data2, options),
+      yAxisID: 'y-axis-ratio'
+    });
+    if (doEnableAverageLines) {
+      datasets = datasets.concat({
+        label: 'average ratio ' + avgRatio,
+        backgroundColor: 'rgba(0,0,0,0)',
+        pointBorderColor: 'rgba(0,0,0,0)',
+        borderDash: [10, 5],
+        pointHitRadius: 0,
+        data: new Array(numDays).fill(avgRatio),
+        yAxisID: 'y-axis-ratio'
+      });
+    }
+  }
+
+  var
+    lineChartData = {
+      labels: getDateLabels(startDateIndex, endDateIndex, datesWithDataAvailable),
+      datasets: datasets
+    },
+    chartEl = document.getElementById('chartView');
+
+  return new Chart(chartEl, {
+    type: 'line',
+    data: lineChartData,
+    options: getChartConfig(options, doEnableRatioLine, doEnableLine1, doEnableLine2)
+  });
+}
+
+function refresh(data, options) {
+  refresh.data = data;
+
+  if (refresh.chartView) {
+    refresh.chartView.destroy();
+  }
+
+  refresh.chartView = createChartView(data, options);
+
+  return refresh.view;
+}
+
+// To help with debugging
+function getData() {
+  return refresh.data;
+}
+
+function updateChartView(data, options) {
+
+  refresh(data, options);
+
+  // Snippet to update points and animate to new values
+  // myLiveChart.datasets[1].points[indexToUpdate].value = Math.random() * 100;
+  // myLiveChart.update();
+
+  return false;
+}
---
+++ store/timeline/js/control.js
@@ -0,0 +1,490 @@
+/**
+ * View controller
+ */
+
+// TODO alarms, e.g. Fullerton hiding badge in IE
+
+'use strict';
+
+var SAME_OPTION_NAME = '<same>';
+
+// Get a parameter value fro the URL query
+function getStringParameterByName(name) {
+  var regex = new RegExp('[\\?&]' + name + '=([^&#]*)'),
+    results = regex.exec(location.search);
+  return results ? decodeURIComponent(results[1].replace(/\+/g, ' ')) : undefined;
+}
+
+function getBooleanParameterByName(name) {
+  var stringVal = getStringParameterByName(name);
+  return stringVal && stringVal === 'true';
+}
+
+function changeUrl(title, url) {
+  if (typeof (history.pushState) != "undefined") {
+    var obj = {title: title, URL: url};
+    history.pushState(obj, obj.title, obj.URL);
+    document.title = title;
+  }
+}
+
+function updateUrlAndTitle(options) {
+  function getCurrentLocationWithoutParams() {
+    var currLoc = window.location.toString();
+
+    return currLoc.substr(0, currLoc.length - window.location.search.length);
+  }
+
+  var params = [];
+  Object.keys(options).forEach(function(optionName) {
+    params.push(encodeURIComponent(optionName) + '=' + encodeURIComponent(options[optionName]));
+  });
+
+  var href = getCurrentLocationWithoutParams() + '?' + params.join('&'),
+    title = 'Sitecues ' + getLabel(options, '1') + ' vs ' + getLabel(options, '2');
+
+  changeUrl(title, href);
+}
+
+function getDefaultParameterMap() {
+  var BEGINNING_OF_TIME = '01/26/2016';
+  return {
+    doEnableLine1: true,
+    doEnableLine2: true,
+    event1: 'page-visited::nonbounce',
+    event2: 'badge-hovered',
+    ua1: '@supported',
+    ua2: SAME_OPTION_NAME,
+    loc1: '@long-running-customers',
+    loc2: SAME_OPTION_NAME,
+    startDate: BEGINNING_OF_TIME,
+    endDate: '',
+    doSmooth: true,
+    doUltraSmooth: false,
+    doStretch: false,
+  }
+}
+
+function getParameterMap() {
+  var defaultParams = getDefaultParameterMap(),
+    params = {
+      doEnableLine1: getBooleanParameterByName('doEnableLine1'),
+      doEnableLine2: getBooleanParameterByName('doEnableLine2'),
+      event1: getStringParameterByName('event1'),
+      event2: getStringParameterByName('event2'),
+      ua1: getStringParameterByName('ua1'),
+      ua2: getStringParameterByName('ua2'),
+      loc1: getStringParameterByName('loc1'),
+      loc2: getStringParameterByName('loc2'),
+      startDate: getStringParameterByName('startDate'),
+      endDate: getStringParameterByName('endDate'),
+      doSmooth: getBooleanParameterByName('doSmooth'),
+      doUltraSmooth: getBooleanParameterByName('doUltraSmooth'),
+      doStretch: getBooleanParameterByName('doStretch')
+    };
+
+  return $.extend({}, defaultParams, params);
+}
+
+function getStringValue(id) {
+  return $('#' + id).val();
+}
+
+function getBooleanValue(id) {
+  return $('#' + id).is(':checked');
+}
+
+function getChartOptions(doConvertSame) {
+  var
+    event1 = getStringValue('event1'),
+    event2 = getStringValue('event2'),
+    ua1 = getStringValue('ua1'),
+    ua2 = getStringValue('ua2'),
+    loc1 = getStringValue('loc1'),
+    loc2 = getStringValue('loc2');
+
+  return {
+    doEnableLine1: getBooleanValue('doEnableLine1'),
+    doEnableLine2: getBooleanValue('doEnableLine2'),
+    event1: event1,
+    event2: doConvertSame && event2 === SAME_OPTION_NAME ? event1 : event2,
+    ua1: ua1,
+    ua2: doConvertSame && ua2 === SAME_OPTION_NAME ? ua1 : ua2,
+    loc1: loc1,
+    loc2: doConvertSame && loc2 === SAME_OPTION_NAME ? loc1 : loc2,
+    startDate: getStringValue('startDate'),
+    endDate: getStringValue('endDate'),
+    doSmooth: getBooleanValue('doSmooth'),
+    doUltraSmooth: getBooleanValue('doUltraSmooth'),
+    doStretch: getBooleanValue('doStretch')
+  };
+}
+
+function convertSameOptions(chartOptions) {
+  var newOptions = $.extend({}, chartOptions);
+  if (newOptions.event2 === SAME_OPTION_NAME) {
+    newOptions.event2 = newOptions.event1;
+  }
+  if (newOptions.ua2 === SAME_OPTION_NAME) {
+    newOptions.ua2 = newOptions.ua1;
+  }
+  if (newOptions.loc2 === SAME_OPTION_NAME) {
+    newOptions.loc2 = newOptions.loc1;
+  }
+  return newOptions;
+}
+
+function changeStringValue(id, val) {
+  var $formControl = $('#' + id);
+  $formControl.val(val);
+  if ($formControl.is('select')) {
+    var $possibleInput = $formControl.next().children().first();
+    if ($possibleInput.is('input')) {
+      $possibleInput.val(val);
+      $possibleInput.css('color', val === SAME_OPTION_NAME ? 'green' : ''); // Color <same> as green
+    }
+  }
+
+}
+
+function changeBooleanValue(id, isChecked) {
+  $('#' + id).prop('checked', isChecked);
+}
+
+// Inits non-combo box defaults which have to be done in a different place
+function setFormValues(paramMap) {
+  changeBooleanValue('doEnableLine1', paramMap.doEnableLine1);
+  changeBooleanValue('doEnableLine2', paramMap.doEnableLine2);
+  changeStringValue('event1', paramMap.event1);
+  changeStringValue('event2', paramMap.event2);
+  changeStringValue('ua1', paramMap.ua1);
+  changeStringValue('ua2', paramMap.ua2);
+  changeStringValue('loc1', paramMap.loc1);
+  changeStringValue('loc2', paramMap.loc2);
+  changeStringValue('startDate', paramMap.startDate);
+  changeStringValue('endDate', paramMap.endDate);
+  changeBooleanValue('doSmooth', paramMap.doSmooth);
+  changeBooleanValue('doUltraSmooth', paramMap.doUltraSmooth);
+  changeBooleanValue('doStretch', paramMap.doStretch);
+}
+
+function ensureValidCheckboxOptions() {
+  if ($('#doUltraSmooth').is(':checked')) {
+    $('#doSmooth').prop('checked', true);
+  }
+}
+
+function onDataAvailable(data) {
+  initOptions(data);
+  populateFormWithValues();
+  listenForUserActions(data);
+  prettify();
+
+  // Show form
+  $('body').addClass('ready');
+
+  // Show current visualization
+  updateChartView(data, convertSameOptions(getChartOptions()));
+}
+
+function getSelectFromTextField(textField) {
+  var $select = $(textField).parent().parent().find('select');
+  return $select[0];
+}
+
+// Ratios make sense when the second line only changes one variable
+function changeOtherLine2ItemsToSame(event) {
+  var selectElem = getSelectFromTextField(event.target),
+    selectId = selectElem.id;
+  if (!selectId) {
+    return;
+  }
+
+  var currVal = $('#' + selectId).val();
+  if (currVal === SAME_OPTION_NAME) {
+    return;
+  }
+
+  $(event.target).css('color', '');
+
+  if (selectId === 'event2') {
+    changeStringValue('ua2', SAME_OPTION_NAME);
+    changeStringValue('loc2', SAME_OPTION_NAME);
+  }
+  else if (selectId === 'ua2') {
+    changeStringValue('event2', SAME_OPTION_NAME);
+    changeStringValue('loc2', SAME_OPTION_NAME);
+  }
+  else if (selectId === 'loc2') {
+    changeStringValue('event2', SAME_OPTION_NAME);
+    changeStringValue('ua2', SAME_OPTION_NAME);
+  }
+}
+
+function listenForUserActions(data) {
+
+  function onHistoryChange() {
+    setFormValues(getParameterMap());
+    updateChartView(data, convertSameOptions(getChartOptions()));
+  }
+
+  window.addEventListener('popstate', onHistoryChange);
+
+  function onFormChange() {
+    var options = getChartOptions();
+    updateUrlAndTitle(options);
+    updateChartView(data, convertSameOptions(options));
+  }
+
+  // If ultra smooth is checked, smooth must be as well
+  $('#doUltraSmooth').on('click', ensureValidCheckboxOptions);
+  $('#doSmooth').on('click', function() {
+    if ($('#doSmooth').is(':checked') === false) {
+      $('#doUltraSmooth').prop('checked', false);
+    }
+  });
+
+  $('#reset').on('click', function() {
+    setFormValues(getDefaultParameterMap());
+  });
+
+  $('#line2-controller').on('autocompleteselect', changeOtherLine2ItemsToSame);
+
+  // Listen for changes
+  $(window).on('submit change autocompleteselect', function() {
+    setTimeout(onFormChange, 0);  // Wait until <same> set on other line 2 items
+  });
+}
+
+// Make the UI more attractive and visually consistent
+function prettify() {
+  // Use jQuery UI tooltips
+  $(document).tooltip();
+
+  // Use jQuery UI button
+  $('#reset').button();
+
+  // Make native inputs have similar size and font
+  // Select text on focus
+  $('input')
+    .addClass('ui-widget ui-widget-content ui-corner-all')
+    .on('focus', function(evt) {
+      evt.target.select();
+    });
+}
+
+function populateFormWithValues() {
+  setFormValues(getParameterMap());
+  ensureValidCheckboxOptions();
+}
+
+function initOptions(data) {
+  initEventOptions(data.eventTotals.byNameOnly);
+  initUserAgentOptions(data.eventTotals.byUserAgentOnly);
+  initLocationOptions(data.siteInfo.locationToSiteIdMap, data.siteInfo.siteIdToLocationsMap);
+  initDatePickers();
+}
+
+function createOption(optionName, readableName) {
+  return $('<option></option>')
+    .attr('value', optionName).text(readableName || optionName);
+}
+
+// Make sure IE10 > IE9
+function alphaNumComparator(a, b) {
+  var END_DIGIT_REGEX = /(\w)(\d)$/;
+  function leadingZeroForEndDigit(s) {
+    return s.replace(END_DIGIT_REGEX, '$10$2');
+  }
+  return leadingZeroForEndDigit(a) > leadingZeroForEndDigit(b) ? 1 : -1;
+}
+
+function initUserAgentOptions(userAgentTotals) {
+  var userAgentNames = Object.keys(userAgentTotals).sort(alphaNumComparator),
+    $uaSelects = $('.ua-chooser');
+
+  $('#ua2').append(createOption(SAME_OPTION_NAME));
+
+  userAgentNames.forEach(function(eventName) {
+    $uaSelects.each(function() {
+      var option = createOption(eventName);
+      $(this).append(option);
+    });
+  });
+
+  $('#ua1')
+    .combobox();
+  $('#ua2')
+    .combobox();
+}
+
+// Sort alphabetically except for key-command or panel-clicked (show at end)
+function eventNameComparator(event1, event2) {
+  function getEventName(eventName) {
+    return eventName
+      .replace(/^key-command::/, 'zzy::')
+      .replace(/^panel-clicked::/, 'zzz::');
+  }
+  return getEventName(event1) > getEventName(event2) ? 1 : -1;
+}
+
+function initEventOptions(allEventTotals) {
+  var allEventNames = Object.keys(allEventTotals).sort(eventNameComparator),
+    $eventNameSelects = $('.event-chooser');
+
+  $('#event2').append(createOption(SAME_OPTION_NAME));
+
+  allEventNames.forEach(function(eventName) {
+    $eventNameSelects.each(function() {
+      var option = createOption(eventName);
+      $(this).append(option);
+    });
+  });
+
+  $('#event1')
+    .combobox();
+  $('#event2')
+    .combobox();
+}
+
+function getNumPageVisits(locationToSiteIdMap, locationName) {
+  var siteIdToPageVisitsMap = locationToSiteIdMap[locationName];
+
+  if (!siteIdToPageVisitsMap) {
+    return;
+  }
+
+  var siteIdsForLocation = Object.keys(siteIdToPageVisitsMap),
+    index = siteIdsForLocation.length,
+    totalPageVisits = 0,
+    currentSiteId;
+
+  while (index --) {
+    currentSiteId = siteIdsForLocation[index];
+    totalPageVisits += siteIdToPageVisitsMap[currentSiteId];
+  }
+
+  return totalPageVisits;
+}
+
+function getReadableNameForLocation(locationToPageVisitsMap, location, totalPageVisits) {
+  function isTLDOrGroup(location) {
+    var firstChar = location.charAt(0);
+    return firstChar === '.' || firstChar === '@';
+  }
+
+  function isInterestingLocation(subLocation) {
+    var PAGE_VISIT_THRESHOLD = 0;
+    return subLocation !== location &&
+      !isTLDOrGroup(subLocation) &&
+      locationToPageVisitsMap[subLocation] > PAGE_VISIT_THRESHOLD ;
+  }
+
+  function pageVisitsComparator(a, b) {
+    return locationToPageVisitsMap[b] - locationToPageVisitsMap[a];
+  }
+
+  function addPageVisits(locationName) {
+    return locationName.split(' ')[0] + ':' + locationToPageVisitsMap[locationName].toLocaleString();
+  }
+
+  var locationNames = [];
+  if (locationToPageVisitsMap && !isTLDOrGroup(location)) {
+    locationNames = Object.keys(locationToPageVisitsMap || {})
+      .filter(isInterestingLocation)
+      .sort(pageVisitsComparator);
+  }
+
+  if (!locationNames.length) {
+    return location + ':' + totalPageVisits.toLocaleString();
+  }
+
+  return location + ' => ' + locationNames.map(addPageVisits).join(', ');
+}
+
+function locationNameComparator(locA, locB) {
+  var isAGroup = locA.charAt(0) === '@',
+    isBGroup = locB.charAt(0) === '@';
+  if (isAGroup !== isBGroup) {
+    return (+isBGroup) - (+isAGroup);
+  }
+  else {
+    return locB > locA ? -1 : 1;
+  }
+}
+
+function initLocationOptions(locationToSiteIdMap, siteIdToLocationsMap) {
+  var allLocations = Object.keys(locationToSiteIdMap)
+      .sort(locationNameComparator)
+      .filter(function(locName) { return locName.indexOf('#s-????????') < 0; }),
+    $locationSelects = $('.location-chooser'),
+    PAGE_VISIT_THRESHOLD = 100, // Don't list locations with fewer than this # of page visits
+    SITE_ID_REGEX = /^#s-[\da-f\?]{8}$/;   // Currently no use
+
+  $('#loc2').append(createOption(SAME_OPTION_NAME));
+
+  allLocations.forEach(function(locationName) {
+    var readableName = locationName,
+      totalPageVisits = getNumPageVisits(locationToSiteIdMap, locationName),
+      locationToPageVisitsMap;
+
+    if (totalPageVisits < PAGE_VISIT_THRESHOLD) {
+      return;  // Otherwise we list too many
+    }
+
+    locationToPageVisitsMap = locationName.match(SITE_ID_REGEX) ? siteIdToLocationsMap : locationToSiteIdMap;
+    readableName = getReadableNameForLocation(locationToPageVisitsMap[locationName], locationName, totalPageVisits);
+    $locationSelects.each(function () {
+      var option = createOption(locationName, readableName);
+      $(this).append(option);
+    });
+  });
+
+  $('#loc1')
+    .combobox();
+  $('#loc2')
+    .combobox();
+}
+
+function initDatePickers() {
+  $('.date')
+    .datepicker({
+      minDate: '01/26/2016',
+      maxDate: 0
+    });
+}
+
+function onReady() {
+  $('#security').one('submit', loadData);
+}
+
+function onError(statusCode, textStatus) {
+  console.log(statusCode);
+  console.log(textStatus);
+  $('body').addClass('error');
+  $('#error').text('An error occurred: ' + statusCode + ' ' + textStatus);
+}
+
+function loadData(submitEvent) {
+  $('body').addClass('password-entered');
+
+  var username = 'sitecues',
+    password = $('#password').val(),
+    // webServiceUrl = 'http://localhost:3001/all.json';
+    webServiceUrl = window.location.protocol + '//' + window.location.hostname + ':3001/all.json';
+
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", webServiceUrl, true);
+  xhr.setRequestHeader("Authorization", "Basic " + btoa(username + ':' + password));
+  xhr.onload = function() {
+    if (xhr.status < 400) {
+      onDataAvailable(JSON.parse(xhr.responseText));
+    }
+    else {
+      onError(xhr.status, xhr.statusText);
+    }
+  };
+  xhr.send();
+}
+
+$(document).ready(onReady);
